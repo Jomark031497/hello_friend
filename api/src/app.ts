7dac8b80cfe654370b51a2972c6b3d8d65f60020
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-12-26 02:15:02 +0800

    setup websockets

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,8 +2,17 @@ import express from "express";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { initializeRoutes } from "./routes.js";
 
+import cors from "cors";
+
 export const createApp = () => {
   const app = express();
+
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+    })
+  );
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,27 @@
 import { logger } from "./utils/logger.js";
 import { createApp } from "./app.js";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 async function main() {
   const app = createApp();
-  const PORT = process.env.PORT;
+  const server = createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: process.env.CLIENT_URL,
+    },
+  });
 
-  app.listen(8080, () => {
-    logger.info(`server started at http://localhost:${PORT}`);
+  io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+      io.emit("chat message", msg); // Emit the message to all clients
+    });
+
+    socket.on("disconnect", () => {});
+  });
+
+  server.listen(8080, () => {
+    logger.info(`server started at http://localhost:${process.env.PORT}`);
   });
 }
 
